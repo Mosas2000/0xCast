b7ea508ed0b18c8f595afaf6e9c64563e928684c
0xMosas <tosinolosasa@gmail.com>
2026-01-29 08:12:57 +0100

    feat(analytics): create analytics hooks index

---
+++ frontend/src/hooks/analytics/index.ts
@@ -0,0 +1,6 @@
+export * from './useAnalytics';
+export * from './usePlatformMetrics';
+export * from './useMarketAnalytics';
+export * from './useUserAnalytics';
+export * from './useVolumeData';
+export * from './useLeaderboard';
