a3003a95eaef022a3fb193accc5e53952b59150a
0xMosas <tosinolosasa@gmail.com>
2026-05-13 09:24:59 +0100

    debug: Replace App with simple Test component to isolate issue

---
+++ frontend/src/Test.tsx
@@ -0,0 +1,5 @@
+export function Test() {
+  return <div style={{ padding: '20px', fontSize: '24px', color: 'red' }}>
+    TEST - If you see this, React is working!
+  </div>;
+}
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,10 +3,10 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import './styles/spacing.css';
 import './i18n/config';
-import App from './App';
+import { Test } from './Test';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Test />
   </StrictMode>
 );
