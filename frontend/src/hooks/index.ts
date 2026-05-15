a5b7a6d2216822d650719ce57525e19995696cec
0xMosas <tosinolosasa@gmail.com>
2026-05-15 17:48:08 +0100

    add barrel exports for improved import ergonomics

---
+++ frontend/src/hooks/index.ts
@@ -0,0 +1,5 @@
+export { useChartState } from './useChartState';
+export { useChartRendering } from './useChartRendering';
+export { useChartIndicators } from './useChartIndicators';
+export { useChartDrawing } from './useChartDrawing';
+export { useRBACStatistics } from './useRBACStatistics';
